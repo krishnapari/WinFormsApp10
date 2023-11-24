181c14d5fc4f8af0e53ae9bba1d73f9c3d0f1b29
krishnapari <kp.bhusa2021@gmail.com>
2023-11-24 23:23:01 +0545

    first commit

--- WinFormsApp10/Form1.Designer.cs
+++ WinFormsApp10/Form1.Designer.cs
@@ -48,6 +48,7 @@
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
--- WinFormsApp10/Form1.cs
+++ WinFormsApp10/Form1.cs
@@ -6,5 +6,10 @@ namespace WinFormsApp10
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
